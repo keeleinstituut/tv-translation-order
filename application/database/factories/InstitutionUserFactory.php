7f39e00e3766e245b43820e56dcb108363da7406
Markus Peterson <Markus Peterson>
2023-05-22 15:51:31 +0300

    add factories for institutionuser and vendor models

---
+++ application/database/factories/InstitutionUserFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Foundation\Testing\WithFaker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\InstitutionUser>
+ */
+class InstitutionUserFactory extends Factory
+{
+    use WithFaker;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => fake()->unique()->uuid(),
+            'institution_id' => fake()->unique()->uuid(),
+            'forename' => fake()->firstName(),
+            'surname' => fake()->lastName(),
+            'personal_identification_code' => $this->faker->unique()->estonianPIC(),
+            'status' => fake()->randomElement(['ACTIVATED']),
+            'phone' => fake()->phoneNumber,
+            'synced_at' => fake()->dateTime(),
+        ];
+    }
+}
