6a912b09675f63a6c45e08291b5965f079d75bcd
dmitriy <dmitriy@singleton.ee>
2023-08-14 18:16:58 +0300

    fix(#431) - pint style fixes

--- application/database/migrations/2023_08_14_151322_alter_collation_for_cached_classfier_values_name_column.php
+++ application/database/migrations/2023_08_14_151322_alter_collation_for_cached_classfier_values_name_column.php
@@ -1,8 +1,5 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use SyncTools\Database\Helpers\BaseCachedEntityTableMigration;
 
 return new class extends BaseCachedEntityTableMigration
