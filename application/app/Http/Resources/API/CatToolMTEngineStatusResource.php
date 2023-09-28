81a1cef41b83aadad162783745b99cd9b3ff9c49
dmitriy <dmitriy@singleton.ee>
2023-09-28 14:13:11 +0300

    feat(#388) - Fix swagger docs

--- application/app/Http/Resources/API/CatToolMTEngineStatusResource.php
+++ application/app/Http/Resources/API/CatToolMTEngineStatusResource.php
@@ -5,10 +5,20 @@ namespace App\Http\Resources\API;
 use App\Models\SubProject;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use OpenApi\Attributes as OA;
 
 /**
  * @mixin SubProject
  */
+#[OA\Schema(
+    required: [
+        'mt_enabled',
+    ],
+    properties: [
+        new OA\Property(property: 'mt_enabled', type: 'boolean'),
+    ],
+    type: 'object'
+)]
 class CatToolMTEngineStatusResource extends JsonResource
 {
     /**
