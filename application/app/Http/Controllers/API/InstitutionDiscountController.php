6220c6298f0bc79e0af0f40591304b0e9ddb4f1c
dmitriy <dmitriy@singleton.ee>
2023-10-10 12:58:22 +0300

    hotfix - fix for institution dicounts update endpoint

--- application/app/Http/Controllers/API/InstitutionDiscountController.php
+++ application/app/Http/Controllers/API/InstitutionDiscountController.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use OpenApi\Attributes as OA;
 use App\Http\OpenApiHelpers as OAH;
+use Throwable;
 
 class InstitutionDiscountController extends Controller
 {
@@ -40,6 +41,7 @@ class InstitutionDiscountController extends Controller
 
     /**
      * @throws AuthorizationException
+     * @throws Throwable
      */
     #[OA\Put(
         path: '/institution-discounts',
@@ -52,18 +54,19 @@ class InstitutionDiscountController extends Controller
     {
         Gate::allowIf(Auth::hasPrivilege(PrivilegeKey::EditInstitutionPriceRate->value));
 
-        $institutionDiscount = self::getInstitution()->institutionDiscount()
-            ->updateOrCreate($request->validated());
+        $institution = self::getInstitution();
+        $institutionDiscount = $institution->institutionDiscount()->firstOrNew();
+        $institutionDiscount->fill([
+            ...$request->validated(),
+            'institution_id' => $institution->id
+        ]);
+        $institutionDiscount->saveOrFail();
 
-        return InstitutionDiscountResource::make($institutionDiscount);
+        return InstitutionDiscountResource::make($institutionDiscount->refresh());
     }
 
     private static function getInstitution(): Institution
     {
-        if (!$institution = Institution::find(Auth::user()->institutionId)) {
-            abort(401);
-        }
-
-        return $institution;
+        return Institution::findOrFail(Auth::user()->institutionId);
     }
 }
