29ebbd49b8980a9fa13511bca672c85db47bbec5
dmitriy <dmitriy@singleton.ee>
2024-09-25 15:21:01 +0300

    feat(#799) - add showing of proper price in a candidate list in case if a candidate is assignee

--- application/app/Http/Resources/API/CandidateResource.php
+++ application/app/Http/Resources/API/CandidateResource.php
@@ -3,7 +3,9 @@
 namespace App\Http\Resources\API;
 
 use App\Models\Candidate;
+use App\Services\Prices\AssigneePriceCalculator;
 use App\Services\Prices\CandidatePriceCalculator;
+use App\Services\Prices\PriceCalculator;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -23,10 +25,21 @@ class CandidateResource extends JsonResource
             'vendor' => VendorResource::make($this->vendor),
             'status' => $this->status,
             'price' => filled($this->vendor) ?
-                (new CandidatePriceCalculator(
-                    $this->assignment,
-                    $this->vendor
-                ))->getPrice() : null,
+                $this->getPriceCalculator()
+                    ->getPrice() : null,
         ];
+    }
+
+    private function getPriceCalculator(): PriceCalculator
+    {
+        if (filled($this->assignment->assigned_vendor_id) &&
+            $this->assignment->assigned_vendor_id === $this->vendor?->id) {
+            return new AssigneePriceCalculator($this->assignment);
+        }
+
+        return new CandidatePriceCalculator(
+            $this->assignment,
+            $this->vendor
+        );
     }
 }
