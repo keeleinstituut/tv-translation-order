eb9307d87277860367cf043bfdad46cd191673b1
dmytro <dmytro@fluidum.ee>
2025-12-15 09:11:25 +0200

    Fix main-user-init.sql

--- main-user-init.sql
+++ main-user-init.sql
@@ -1,6 +1,7 @@
 \connect app;
 GRANT CREATE ON SCHEMA public TO app_user;
 GRANT USAGE ON SCHEMA public TO app_user;
+
 GRANT ALL ON ALL TABLES IN SCHEMA public TO app_user;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO app_user;
 GRANT ALL ON ALL PROCEDURES IN SCHEMA public TO app_user;
@@ -12,13 +13,11 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON ROUTINES TO app_user;
 
-ALTER SCHEMA application OWNER TO app_user;
 ALTER SCHEMA entity_cache OWNER TO app_user;
-ALTER SCHEMA public OWNER TO app_user;
 
--- ALTER TABLE entity_cache.cached_classifier_values OWNER TO app_user;
--- ALTER TABLE entity_cache.cached_institutions OWNER TO app_user;
--- ALTER TABLE entity_cache.cached_institution_users OWNER TO app_user;
+ALTER TABLE entity_cache.cached_classifier_values OWNER TO app_user;
+ALTER TABLE entity_cache.cached_institutions OWNER TO app_user;
+ALTER TABLE entity_cache.cached_institution_users OWNER TO app_user;
 
 \connect testing;
 GRANT CREATE ON SCHEMA public TO app_user;
@@ -34,10 +33,8 @@ ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO app_user;
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON ROUTINES TO app_user;
 
-ALTER SCHEMA application OWNER TO app_user;
 ALTER SCHEMA entity_cache OWNER TO app_user;
-ALTER SCHEMA public OWNER TO app_user;
 
--- ALTER TABLE entity_cache.cached_classifier_values OWNER TO app_user;
--- ALTER TABLE entity_cache.cached_institutions OWNER TO app_user;
--- ALTER TABLE entity_cache.cached_institution_users OWNER TO app_user;
+ALTER TABLE entity_cache.cached_classifier_values OWNER TO app_user;
+ALTER TABLE entity_cache.cached_institutions OWNER TO app_user;
+ALTER TABLE entity_cache.cached_institution_users OWNER TO app_user;
